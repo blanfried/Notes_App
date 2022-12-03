d113075e98d4b22a63072eb776357c32c79c9a44
Baer Lanfried <blanfried@yahoo.com>
2022-12-03 20:45:07 +0000

    added multiple notes functions (most importantly getNotes, saveNote, and deleteNote) so that user can interact with notes in a varaiety of ways

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -11,3 +11,42 @@ if (window.location.pathname === "/notes") {
   newNoteBtn = document.querySelector(".new-note");
   noteList = document.querySelectorAll(".list-container .list-group");
 }
+
+// Show an element
+const show = (elem) => {
+      elem.style.display = "inline";
+    };
+    
+    // Hide an element
+    const hide = (elem) => {
+      elem.style.display = "none";
+    };
+    
+    // activeNote is used to keep track of the note in the textarea
+    let activeNote = {};
+    
+    const getNotes = () =>
+      fetch("/api/notes", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    
+    const saveNote = (note) =>
+      fetch("/api/notes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note),
+      });
+    
+    const deleteNote = (id) =>
+      fetch(`/api/notes/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: id,
+      });
